9ff2e86e83172a22920de6af15944338432d32b6
oyerohabib <oyerohabib@yahoo.com>
2021-08-14 13:01:38 +0900

    completed tabs project

--- 1. birthdayreminder/src/App.js
+++ 1. birthdayreminder/src/App.js
@@ -1,17 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
-import List from './List';
 function App() {
-  const [people, setPeople] = useState(data)
-  return <>
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => {setPeople([])}}>Clear All</button>
-      </section>
-    </main>
-  </>
+  return <h2>slider project setup</h2>;
 }
 
 export default App;
